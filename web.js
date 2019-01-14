69e50480607a736b2e2bc1c25a0e1be2d212fdc2
rana <46030702+Afnan2000@users.noreply.github.com>
2019-01-14 12:23:25 +0000

    first commit

---
+++ web.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const filePath = 'sensorlog.txt';
+const csvtojson = require("csvtojson");
+
+function handle_incoming_request(req, res) {
+    console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+
+    csvtojson().fromFile(filePath).then(function(jsonObj) {
+        res.writeHead(200, {"Content-Type":"application/json"});
+        res.end(JSON.stringify(jsonObj));
+    });
+}
+
+var s = http.createServer(handle_incoming_request); //
+
+s.listen(8080);
